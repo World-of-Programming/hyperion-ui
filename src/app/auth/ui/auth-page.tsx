49c989e4caaf4b7dde196a6d15725fd7a204539d
Cyan512 <182935@unsaac.edu.pe>
2026-02-20 21:30:37 -0500

    refactor  auth page

--- src/app/auth/ui/auth-page.tsx
+++ src/app/auth/ui/auth-page.tsx
@@ -2,8 +2,8 @@ import LoginForm from "./login-form";
 
 export default function AuthPage() {
     return (
-        <>
+        <div>
             <LoginForm/>
-        </>
+        </div>
     )
 }
